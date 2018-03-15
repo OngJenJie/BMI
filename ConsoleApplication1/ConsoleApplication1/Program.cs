1b8997e0ae5a6b5757b06e6ff54ec340882bcf59
m305-16\w401 <asd123@samail.com>
2018-03-15 17:50:48 +0800

    基本BMI程式码完成还没加入IF

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "BMI计算器";
+            Console.WriteLine("请输入您的身高：");
+            string heigh = Console.ReadLine();
+            Console.WriteLine("请输入您的体重:");
+            string weight = Console.ReadLine();
+            double BMI = double.Parse(heigh) / double.Parse(weight) * double.Parse(weight);
+            Console.WriteLine("您的BMI值是: " + BMI );
+            Console.ReadLine();
+        }
+
+    }
+}
